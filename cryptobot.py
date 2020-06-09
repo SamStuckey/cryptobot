8f011a4d1ca65ee2250b4b4250e5be72510b493a
Sam Stuckey <sam.n.stuckey@gmail.com>
2020-06-08 22:01:40 -0400

    WIP

--- cryptobot.py
+++ cryptobot.py
@@ -2,8 +2,8 @@ import shrimpy
 from decouple import config
 import os
 
-public_key = config['SHRIPY_PUBLIC_KEY']
-secret_key = config['SHRIPY_SECRET_KEY']
+public_key = config['SHRIMPY_PUBLIC_KEY']
+secret_key = os.getenv['SHRIMPY_SECRET_KEY']
 print(public_key)
 print(secret_key_key)
 #  client = shrimpy.ShrimpyApiClient(public_key, secret_key)
