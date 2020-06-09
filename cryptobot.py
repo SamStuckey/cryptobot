9012b3b61ff70b2a32ffa6a6688a53f2cf4797b7
Sam Stuckey <sam.n.stuckey@gmail.com>
2020-06-08 20:25:05 -0400

    WIP

--- cryptobot.py
+++ cryptobot.py
@@ -1,7 +1,10 @@
-import shrimpy
+#  import shrimpy
+from decouple import config
 import os
 
-public_key = os.getenv['SHRIPY_PUBLIC_KEY']
-secret_key = os.getenv['SHRIPY_SECRET_KEY']
-client = shrimpy.ShrimpyApiClient(public_key, secret_key)
-ticker = client.get_ticker('bittrex')
+public_key = config['SHRIPY_PUBLIC_KEY']
+secret_key = config['SHRIPY_SECRET_KEY']
+print public_key
+print secret_key_key
+#  client = shrimpy.ShrimpyApiClient(public_key, secret_key)
+#  ticker = client.get_ticker('bittrex')
