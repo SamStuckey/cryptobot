9e74459baef9cf49db10d924ee18ee903b854101
Sam Stuckey <sam.n.stuckey@gmail.com>
2021-02-06 23:36:39 +0000

    wip

--- cbot/__main__.py
+++ cbot/__main__.py
@@ -1,8 +1,8 @@
-from cbot.websocket import Websocket
-ws = Websocket()
-try:
-    ws.start()
-except:
-    ws.close()
-#  from cbot.app import Cbot
-#  Cbot().test_run()
+#  from cbot.websocket import Websocket
+#  ws = Websocket()
+#  try:
+    #  ws.start()
+#  except:
+    #  ws.close()
+from cbot.app import Cbot
+Cbot().test_run()
