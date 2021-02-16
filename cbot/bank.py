e642dfd7c1cc5a7f93e0c1eb2b26afc034fd4ef3
Sam Stuckey <sam.n.stuckey@gmail.com>
2021-02-16 13:09:17 -0500

    remove redundant refresh balance method

--- cbot/bank.py
+++ cbot/bank.py
@@ -8,11 +8,6 @@ class Bank():
         self.usd_balance    = client.usd_balance()
         self.markets        = self._create_coins(markets)
 
-    def refresh_balances(self):
-        self.usd_balance = client.usd_balance()
-        for coin in self.coins:
-            coin.update_balance()
-
     def balances(self, refresh=False):
         blncz = []
         for c in self.coins:
@@ -20,11 +15,6 @@ class Bank():
                 coin.update_balance()
             blncz.append({ 'name': c.market, 'balance': c.balance })
         return blncz
-
-    def _create_coins(self, markets):
-        for market in markets:
-            symbol = market.replace('-USD', '')
-            self.coins.append(Coin(market, symbol, self.client))
 
     def cash_out_value(self):
         return self.total_coin_values() + self.usd_balance
@@ -34,3 +24,9 @@ class Bank():
         for c in self.coins:
             total += float(c.worth())
         return total
+
+    def _create_coins(self, markets):
+        for market in markets:
+            symbol = market.replace('-USD', '')
+            self.coins.append(Coin(market, symbol, self.client))
+
